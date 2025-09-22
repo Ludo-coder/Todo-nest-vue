cda74df47a300013ac6dba6ba71dbd4cb38eb87c
Ludo Hund <Ludovic_hund@hotmail.fr>
2025-09-22 17:56:40 +0200

    feat(router): adding router

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import LoginPage from "@/pages/LoginPage.vue";
+import HomePage from "@/pages/HomePage.vue";
+import MainLayout from "@/layouts/MainLayout.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Login",
+    component: LoginPage,
+  },
+  {
+    path: "/home",
+    component: MainLayout,
+    children: [
+      {
+        path: "",
+        name: "Home",
+        component: HomePage,
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.VUE_APP_BASE_URL),
+  routes,
+});
+
+router.beforeEach((to, _from, next) => {
+  const isAuthenticated = Boolean(localStorage.getItem("access_token"));
+  if (to.name !== "/" && !isAuthenticated) next({ name: "/" });
+  else next();
+});
+
+export default router;
